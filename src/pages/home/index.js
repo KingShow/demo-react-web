8dca742308707a5be398f0b91b09b2dc8168b9ff
KingShow <1318389022@qq.com>
2018-03-21 13:57:42 +0800

    增加 prop-types

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,34 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { addTodo } from '../../actions/index';
+import { Input } from 'antd';
 import Content from 'components/content';
 
 class AppHome extends Component {
-
+    static propTypes = {
+        todos: PropTypes.object.isRequired,
+        addTodo: PropTypes.func.isRequired,
+    }
+    state = {
+        input_text: '',
+    }
+    handleInputChange = (e) => {
+        this.setState({
+          input_text: e.target.value,
+        });
+    }
+    onTodoClick = () => {
+        this.props.addTodo(this.state.input_text);
+    }
     render() {
-        const {todos, onTodoClick} = this.props;
+        const {todos} = this.props;
         const listItems = todos.todos.map((item, key) => {
             return <li key={key}>{item.text}</li>
         });
         return (
             <Content>
                 <ul>{listItems}</ul>
-                <button onClick={() => onTodoClick('two')}>add todo</button>  
+                <Input 
+                placeholder="基本使用" 
+                value={this.state.input_text} 
+                onChange={this.handleInputChange}/>
+                <button onClick={this.onTodoClick}>add</button>  
             </Content>
         )
     }
 }
-const mapStateToProps = state => {
-    return {
-        ...state,
-    }
-};
-const mapDispatchToProps = dispatch => {
-    return {
-        onTodoClick: text => {
-            dispatch(addTodo(text))
-        }
-    }
-}
-const Home =  connect(mapStateToProps, mapDispatchToProps)(AppHome);
+const Home =  connect(state => ({
+    ...state,
+}), {
+    addTodo,
+})(AppHome);
 export default Home;
